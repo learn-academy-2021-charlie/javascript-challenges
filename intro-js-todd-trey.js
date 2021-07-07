2dd9cda60e37d9ff39fe609fe6cf1837c5005d41
ToddComte <comtetodd@gmail.com>
2021-07-07 17:22:40 -0600

    completed 11 - 14

--- intro-js-todd-trey.js
+++ intro-js-todd-trey.js
@@ -15,23 +15,23 @@ console.log(6 % 33)
 // Write the code that will log the length of a string containing your name.
 console.log("todd and trey".length)
 // Write the code that will log whether your string includes the letter "e"?
-console.log ((mystring.indexOf("e"))
+console.log ("mytrey".indexOf("e"))
 // Write the code that will log the character at the first index of the string.
-console.log(mystring[0])
+console.log("mytrey"[0])
 // Write the code that will log the string in all uppercase letters.
-console.log(mystring.toUpperCase())
+console.log("mytodd".toUpperCase())
 // Write the code that will log true or false for the following:
 
 // Is 34 divided by 3 greater than 67 divided by 2?
 console.log((34 / 3) >(67 / 2))
 // Does 5 evaluate to the same as "5"?
-
+console.log(5 === "5")
 // Does 5 strictly equal "5"?
-
+console.log(5 === "5")
 // Does !3 strictly equal 3?
-
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
-
+console.log("LEARN".length === 5 && "student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
 
 // Does "LEARN" contain the subset "RN"?
