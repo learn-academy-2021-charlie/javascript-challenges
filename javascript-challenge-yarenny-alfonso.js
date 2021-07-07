b9df9ca2c47ed0bb12d836fdc3cec72d58f3ed8e
byarenny <byarenny@gmail.com>
2021-07-07 15:51:00 -0700

    challenges 11-20 completed

--- javascript-challenge-yarenny-alfonso.js
+++ javascript-challenge-yarenny-alfonso.js
@@ -20,3 +20,25 @@ console.log(myName.includes("e"))
 console.log(myName.charAt(0))
 // Write the code that will log the string in all uppercase letters.
 console.log(myName.toUpperCase())
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34/3 > 67/2)
+// Does 5 evaluate to the same as "5"?
+console.log(5=="5")
+// Does 5 strictly equal "5"?
+console.log(5==="5")
+// Does !3 strictly equal 3?
+console.log(!3===3)
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+var learn="LEARN"
+var student="Student"
+console.log(learn.length===5 && student.length===7)
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log(learn.length===5 || student.length===10)
+// Does "LEARN" contain the subset "RN"?
+console.log(learn.includes("RN"))
+// Does "LEARN" contain the subset "rn"?
+console.log(learn.includes("rn"))
+// Does "LEARN"[0] strictly equal "l"?
+console.log(learn[0]==="l")
+// Modify the code from the previous question to return true.
+console.log(learn[0]==="L")
