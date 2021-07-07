751c01e154d5e0ac075f408f5d540458f51a61fb
charper95 <charper1995@gmail.com>
2021-07-07 15:40:36 -0700

    answered some questions

--- js-intro-conrad-milton.js
+++ js-intro-conrad-milton.js
@@ -8,12 +8,20 @@ console.log(123-67)
 // Write the code that will log the outcome of 56 multiplied by 23.
 console.log(56*23)
 // Write the code that will log the outcome of 45 divided by 5.
+console.log(45/5)
 // Write the code that will log the outcome of 5 to the power of 7.
+console.log(5**7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33%6)
 // Write the code that will log the length of a string containing your name.
+console.log("conrad".length)
 // Write the code that will log whether your string includes the letter "e"?
+console.log("harper".includes("e"))
 // Write the code that will log the character at the first index of the string.
+console.log("string"[0])
 // Write the code that will log the string in all uppercase letters.
+console.log("uppercase".toUpperCase())
+
 // Write the code that will log true or false for the following:
 //
 // Is 34 divided by 3 greater than 67 divided by 2?
@@ -26,6 +34,7 @@ console.log(56*23)
 // Does "LEARN" contain the subset "rn"?
 // Does "LEARN"[0] strictly equal "l"?
 // Modify the code from the previous question to return true.
+
 // Consider the variables:
 //
 // var theQuestion = "life, the universe, and everything"
