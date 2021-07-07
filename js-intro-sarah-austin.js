61987e73803c18d52335f66f1ae25e0af4a84bd6
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-07-07 16:13:19 -0700

    jared and arturo

---
+++ js-intro-sarah-austin.js
@@ -0,0 +1,19 @@
+console.log(34+71);
+console.log(67-123);
+console.log(56*23);
+console.log(45/5);
+console.log(5**7);
+console.log(33/6);
+console.log("Arturo".length);
+console.log("Arturo".indexOf("e"));
+console.log("Arturo".includes("e"));
+console.log("Jared"[0]);
+console.log("jared".toUpperCase());
+console.log(34/3 > 67/2)
+console.log(34/3 < 67/2)
+console.log(5 === "5")
+console.log(5 == "5")
+console.log(!3 === 3)
+console.log(!3)
+console.log("learn".length === 5 && "student".length === 7)
+console.log("learn".length === 5 || "student".length === 10)
