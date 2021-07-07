cf413cd7b9d2023636c58884361aaf5fb31ac310
Bpompa95 <brianpompa02@gmail.com>
2021-07-07 16:46:04 -0700

    complete

--- js-intro-brian-lauralyn.js
+++ js-intro-brian-lauralyn.js
@@ -42,40 +42,46 @@
 // console.log(34/3 > 67/2)
 
 // 12. Does 5 evaluate to the same as "5"?
-console.log()
+// console.log(5 == "5")
 
 // 13. Does 5 strictly equal "5"?
 // console.log(5 === "5")
 
 // 14. Does !3 strictly equal 3?
-console.log()
+// console.log(!3 === 3)
 
 // 15. Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
 // console.log("LEARN".length === 5 && "Student".length === 7)
 
 // 16. Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
-console.log()
+// console.log("LEARN".length === 5 || "Student".length === 10)
 
 // 17. Does "LEARN" contain the subset "RN"?
 // console.log("LEARN".includes("RN"))
 
 // 18. Does "LEARN" contain the subset "rn"?
-console.log()
+// console.log("LEARN".includes("rn"))
 
 // 19. Does "LEARN"[0] strictly equal "l"?
 // console.log("LEARN"[0] === "l")
 
 // 20. Modify the code from the previous question to return true.
-console.log()
+// console.log("LEARN"[0]==="L")
 
 // Consider the variables:
-
-// var theQuestion = "life, the universe, and everything"
-// var theAnswer = 42
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
 // Write the code that will log theAnswer divided by 2.
+// console.log(theAnswer/2)
 // Write the code that will log the length of theQuestion.
+// console.log(theQuestion.length)
 // Write the code that will log the index of the character "f" in the theQuestion.
+// console.log(theQuestion.indexOf("f"))
 // Write the code that will log the concatenation of the two variables.
+// console.log(theQuestion + theAnswer)
 // Write the code that will log "the universe".
+console.log("the universe")
 // Write the code that will log the character "l" from theQuestion.
+// console.log(theQuestion.charAt(0))
 // Write the code that will log whether theQuestion.length is greater that theAnswer.
+console.log(theQuestion.length> theAnswer.length)
