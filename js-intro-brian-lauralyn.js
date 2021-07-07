b05116d478eb3b880ff123fda093ede32c1e0f35
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-07 15:22:58 -0700

    initial commit

---
+++ js-intro-brian-lauralyn.js
@@ -0,0 +1,57 @@
+// 1. Write the code that will log the outcome of 34 added to 71.
+// console.log(34+71)
+
+// 2. Write the code that will log the outcome of 67 subtracted from 123.
+console.log()
+
+// 3. Write the code that will log the outcome of 56 multiplied by 23.
+// console.log(56*23)
+
+// 4. Write the code that will log the outcome of 45 divided by 5.
+console.log()
+
+// 5. Write the code that will log the outcome of 5 to the power of 7.
+// console.log(5**7)
+
+// 6. Write the code that will log the whole number remainder of 33 divided by 6.
+console.log()
+
+// 7. Write the code that will log the length of a string containing your name.
+// console.log("lauralyn".length)
+
+// 8. Write the code that will log whether your string includes the letter "e"?
+console.log()
+
+// 9. Write the code that will log the character at the first index of the string.
+//         012345
+// var str = "string"
+// console.log(str[2])
+
+// 10. Write the code that will log the string in all uppercase letters.
+console.log()
+
+// Write the code that will log true or false for the following:
+
+// 11. Is 34 divided by 3 greater than 67 divided by 2?
+
+// 12. Does 5 evaluate to the same as "5"?
+// 13. Does 5 strictly equal "5"?
+// 14. Does !3 strictly equal 3?
+// 15. Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// 16. Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// 17. Does "LEARN" contain the subset "RN"?
+// 18. Does "LEARN" contain the subset "rn"?
+// 19. Does "LEARN"[0] strictly equal "l"?
+// 20. Modify the code from the previous question to return true.
+
+// Consider the variables:
+
+// var theQuestion = "life, the universe, and everything"
+// var theAnswer = 42
+// Write the code that will log theAnswer divided by 2.
+// Write the code that will log the length of theQuestion.
+// Write the code that will log the index of the character "f" in the theQuestion.
+// Write the code that will log the concatenation of the two variables.
+// Write the code that will log "the universe".
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will log whether theQuestion.length is greater that theAnswer.
