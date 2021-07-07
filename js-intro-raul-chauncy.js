627239166b414a80fc87f4802486c0a72b3733a1
CSapien08 <sapiano08@gmail.com>
2021-07-07 15:26:19 -0700

    Committing problems 1-10

---
+++ js-intro-raul-chauncy.js
@@ -0,0 +1,34 @@
+// 1. Write the code that will log the outcome of 34 added to 71.
+console.log(34 + 71);
+
+// 2.Write the code that will log the outcome of 67 subtracted from 123.
+console.log(67 - 123);
+
+// 3. Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56 * 23);
+
+// 4. Write the code that will log the outcome of 45 divided by 5.
+console.log(45 / 5);
+
+// 5. Write the code that will log the outcome of 5 to the power of 7.
+console.log(5 ** 7);
+
+// 6. Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6);
+
+// 7. Write the code that will log the length of a string containing your name.
+console.log("Chauncy".length);
+
+// 8. Write the code that will log whether your string includes the letter "e"?
+console.log("Chauncy".includes("e"));
+
+// 9. Write the code that will log the character at the first index of the string.
+console.log("Chauncy".charAt(0));
+
+var name = "Chauncy";
+console.log(name[0]);
+
+console.log("Chauncy"[0]);
+
+// 10. Write the code that will log the string in all uppercase letters.
+console.log("Chauncy".toUpperCase());
