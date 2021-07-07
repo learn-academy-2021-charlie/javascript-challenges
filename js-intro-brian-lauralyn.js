7a16c76728c62922c2d4ed9052fb3eb2600c0c19
Bpompa95 <brianpompa02@gmail.com>
2021-07-07 15:54:24 -0700

    1 -10 done

--- js-intro-brian-lauralyn.js
+++ js-intro-brian-lauralyn.js
@@ -2,25 +2,28 @@
 // console.log(34+71)
 
 // 2. Write the code that will log the outcome of 67 subtracted from 123.
-console.log()
+// console.log(123-67)
 
 // 3. Write the code that will log the outcome of 56 multiplied by 23.
 // console.log(56*23)
 
 // 4. Write the code that will log the outcome of 45 divided by 5.
-console.log()
+// console.log(45/5)
 
 // 5. Write the code that will log the outcome of 5 to the power of 7.
 // console.log(5**7)
 
 // 6. Write the code that will log the whole number remainder of 33 divided by 6.
-console.log()
+// console.log(33/6)
 
 // 7. Write the code that will log the length of a string containing your name.
 // console.log("lauralyn".length)
 
 // 8. Write the code that will log whether your string includes the letter "e"?
-console.log()
+// var str = "string"
+// var str2 = "strange"
+// console.log(str.includes("e"))
+// console.log(str2.includes("e"))
 
 // 9. Write the code that will log the character at the first index of the string.
 //         012345
@@ -28,7 +31,8 @@ console.log()
 // console.log(str[2])
 
 // 10. Write the code that will log the string in all uppercase letters.
-console.log()
+var lowercase = "object"
+console.log(lowercase.toUpperCase())
 
 // Write the code that will log true or false for the following:
 
