3998b5ccb79da35223e78d5b8027cdc6c08cad60
Andy19936 <andy19936@gmail.com>
2021-07-07 15:58:07 -0700

    finished product

--- js-intro-thomas-andy.js
+++ js-intro-thomas-andy.js
@@ -43,12 +43,19 @@ console.log("LEARN"[0] === "l");
 console.log("LEARN"[0] !== "l");
 // Consider the variables:
 //
-// var theQuestion = "life, the universe, and everything"
-// var theAnswer = 42
+ var theQuestion = "life, the universe, and everything"
+ var theAnswer = 42
+console.log(theAnswer /2 )
 // Write the code that will log theAnswer divided by 2.
 // Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
 // Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"))
 // Write the code that will log the concatenation of the two variables.
+console.log (theQuestion+" "+theAnswer)
 // Write the code that will log "the universe".
+console.log(theQuestion.slice(6,18) )
 // Write the code that will log the character "l" from theQuestion.
+console.log(theQuestion [0])
 // Write the code that will log whether theQuestion.length is greater that theAnswer.
+console.log(theQuestion.length > theAnswer)
