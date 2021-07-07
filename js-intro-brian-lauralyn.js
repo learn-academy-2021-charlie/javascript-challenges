b8bea25c49fe5b0e98a7b4f73f62c99af6094a0e
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-07 16:19:51 -0700

    11-20 odds done

--- js-intro-brian-lauralyn.js
+++ js-intro-brian-lauralyn.js
@@ -31,22 +31,42 @@
 // console.log(str[2])
 
 // 10. Write the code that will log the string in all uppercase letters.
-var lowercase = "object"
-console.log(lowercase.toUpperCase())
+// var lowercase = "object"
+// console.log(lowercase.toUpperCase())
 
 // Write the code that will log true or false for the following:
 
 // 11. Is 34 divided by 3 greater than 67 divided by 2?
+// console.log(34/3)
+// console.log(67/2)
+// console.log(34/3 > 67/2)
 
 // 12. Does 5 evaluate to the same as "5"?
+console.log()
+
 // 13. Does 5 strictly equal "5"?
+// console.log(5 === "5")
+
 // 14. Does !3 strictly equal 3?
+console.log()
+
 // 15. Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// console.log("LEARN".length === 5 && "Student".length === 7)
+
 // 16. Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log()
+
 // 17. Does "LEARN" contain the subset "RN"?
+// console.log("LEARN".includes("RN"))
+
 // 18. Does "LEARN" contain the subset "rn"?
+console.log()
+
 // 19. Does "LEARN"[0] strictly equal "l"?
+// console.log("LEARN"[0] === "l")
+
 // 20. Modify the code from the previous question to return true.
+console.log()
 
 // Consider the variables:
 
