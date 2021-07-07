8a9488266020209fb0a2b8b414fd2d233b87fe29
celoazevedo <meazevedo86@gmail.com>
2021-07-07 15:58:49 -0700

    complete 11-15

--- javascript-intro-celo-galadoe.js
+++ javascript-intro-celo-galadoe.js
@@ -29,3 +29,20 @@ console.log("galadoe"[0]);
 
 // 10
 console.log("galadoe".toUpperCase());
+
+// 11
+console.log((34/3) > (67/2))
+
+// 12
+console.log(5 == "5")
+
+// 13
+console.log(5 === "5")
+
+// 14
+console.log(!3 === 3)
+
+// 15
+console.log("LEARN".length === 5 && "Student".length === 7)
+
+//
