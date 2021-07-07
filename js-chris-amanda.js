d992790ede06437f68bf4bd758a75e10dd14fd4d
Aferg15 <amanda@afergdesign.com>
2021-07-07 16:03:42 -0700

    challenge items 13-22

--- js-chris-amanda.js
+++ js-chris-amanda.js
@@ -33,3 +33,13 @@ console.log("chris"[0])
 
 var myName = "chris"
 console.log(myName.toUpperCase())
+console.log(34/3 > 67/2)
+console.log(5 === "5")
+console.log(5 == "5")
+console.log(!3 === 3)
+console.log("LEARN".length === 5 && "Student".length === 7)
+console.log("LEARN".length === 5 || "Student".length === 10)
+console.log("LEARN".includes("RN"))
+console.log("LEARN".includes("rn"))
+console.log("LEARN"[0] === "l")
+console.log("LEARN"[0] === "L")
