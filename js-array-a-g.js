e2499448a05473abe75bffdcb658841151035459
galadoe <kgaladoe@gmail.com>
2021-07-08 15:58:13 -0700

    questions 1-4

---
+++ js-array-a-g.js
@@ -0,0 +1,26 @@
+//Array Challenge Questions
+
+//1
+var groceryList = ["chips", "dip", "cookies"]
+groceryList.push("soda")
+console.log(groceryList);
+
+
+//2
+var groceryList2 = groceryList
+groceryList2.push("granola")
+
+console.log(groceryList2);
+
+
+// 3
+groceryList = groceryList.slice(0, 2);
+console.log(groceryList);
+
+
+// 4
+groceryList.unshift("beans");
+console.log(groceryList);
+
+
+// 5
