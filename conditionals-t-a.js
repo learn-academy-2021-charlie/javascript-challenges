393e0bd1ccdba6fc3f686019cf5318329ca51f90
Aferg15 <amanda@afergdesign.com>
2021-07-08 11:00:21 -0700

    challenges updated

--- conditionals-t-a.js
+++ conditionals-t-a.js
@@ -1,13 +1,27 @@
 // Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
 //
-var item = 100
-if(item <= 100){
-  console.log("in budget");
-}
+// // var item = 100
+// if(item <= 100){
+//   console.log("in budget");
+// }
 // Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+// var hungry = false
+// if (hungry === true){
+//   console.log("eat food")
+// }else{
+//   console.log("keep coding")
+// }
 //
 // Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
 //
+// var trafficLight = "red"
+// if (trafficLight === "green"){
+//   console.log("go")
+// }else if (trafficLight === "yellow"){
+//   console.log("slow down")
+// }else {
+//   console.log("stop")
+// }
 // Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
 //
 // Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
