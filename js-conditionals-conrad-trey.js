2fe777477d84c01882b0c6f6cb528bf45c054609
charper95 <charper1995@gmail.com>
2021-07-08 11:24:26 -0700

    answered questions

---
+++ js-conditionals-conrad-trey.js
@@ -0,0 +1,54 @@
+// var cost = 101
+
+// if(cost<=100) {
+//     console.log("in budget")
+// } else if(cost>100) {
+//     console.log("out of budget")
+// } else {
+//     console.log("error needs to be a number")
+// }
+
+// var hungry = false
+
+// if(hungry === true) {
+//     console.log("eat food")
+// } else if(hungry === false) {
+//     console.log("keep coding")
+// } else {
+    
+// }
+
+// var trafficLight = "yellow"
+
+// if(trafficLight === "green") {
+//     console.log("go") 
+// } else if(trafficLight === "yellow") {
+//     console.log("slow down")
+// } else if(trafficLight === "red") {
+//     console.log("stop")
+// } else {
+
+// }
+
+// var num1 = 2
+// var num2 = 2
+
+// if(num1 > num2) {
+//     console.log(`${num1} is greater than ${num2}`)
+// } else if(num1 < num2) {
+//     console.log(`${num2} is greater than ${num1}`)
+// } else if(num1 === num2) {
+//     console.log("The numbers are the same")
+// } else {
+
+// }
+
+var num = 8
+
+if(num === 0) {
+    console.log("zero")
+} else if(num % 2 === 0) {
+    console.log("even")
+} else {
+    console.log("odd")
+}
