d1a3fce1112f050a4c6041d180ec38cc23416714
milch87 <miltonestrada9@gmail.com>
2021-07-08 16:04:35 -0700

    1-4 done

--- js-arrays/js-arrays-milton-lauralyn.js
+++ js-arrays/js-arrays-milton-lauralyn.js
@@ -6,12 +6,23 @@
 var groceryList = ["chips", "dip", "cookies"]
 
 // 1. Write the code that will add "soda" to the end of the original array.
+groceryList.push("soda")
+console.log(groceryList)
 //
 // 2. Write the code that will add "granola" to the end of array without altering the original array.
+var groceryList2 = ["granola"]
+console.log(groceryList.concat(groceryList2))
 //
 // 3. Write the code that will return a subset of the array containing only "chips" and "dip".
+console.log(groceryList.slice(0, 2))
 //
+
 // 4. Write the code that will add "beans" to the "chips" and "dip" array.
+var chipsDip = groceryList.slice(0,2)
+console.log(chipsDip)
+chipsDip.push("beans")
+console.log(chipsDip)
+
 //
 // Consider the variable:
 //
