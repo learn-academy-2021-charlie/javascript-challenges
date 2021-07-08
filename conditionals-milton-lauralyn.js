4656cc9902f1f1a61a0e8d20028b9014e8a53093
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-08 11:35:45 -0700

    1-4 done

--- conditionals-milton-lauralyn.js
+++ conditionals-milton-lauralyn.js
@@ -3,29 +3,55 @@
 
 //
 // 1. Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
-var item = 9
-if (item <= 100)
-{
-  console.log("stays in budget")
-}
-else {
-console.log("not in budget")
-}
+// var item = 9
+// if (item <= 100)
+// {
+//   console.log("stays in budget")
+// }
+// else {
+// console.log("not in budget")
+// }
 
 //
 // 2. Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-var hungry = false
-if (hungry) {
-  console.log("eat food")
-} else {
-  console.log("keep coding")
-}
+// var hungry = false
+// if (hungry) {
+//   console.log("eat food")
+// } else {
+//   console.log("keep coding")
+// }
 //
 // 3. Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// var trafficLight = "green"
+// var trafficLight = "yellow"
+// var trafficLight = "red"
+//
+// if (trafficLight === "green") {
+//   console.log("go")
+// } else if (trafficLight === "yellow") {
+//   console.log("slow down")
+// }
+// else {
+//   console.log("stop")
+// }
+
 
 //
 // 4. Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
 //
+var number1 = 8
+// var number2 = 12
+var number2 = 8
+
+if (number1 > number2) {
+  console.log(number1)
+} else if (number2 > number1) {
+  console.log(number2)
+} else {
+  console.log("equal")
+}
+
+
 // 5. Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
 //
 // STRETCH Challengesa
