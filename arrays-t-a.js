37b7633fafaa6fb5adcee163e99a2453a294d37f
Aferg15 <amanda@afergdesign.com>
2021-07-08 16:05:22 -0700

    third challenge section

--- arrays-t-a.js
+++ arrays-t-a.js
@@ -47,18 +47,27 @@
 // console.log(numbers);
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
-var numbers = [2, 4, 6, 8, 10]
-var randomNum = [0]
-console.log(randomNum.concat(numbers));
+// var numbers = [2, 4, 6, 8, 10]
+// var randomNum = [0]
+// console.log(randomNum.concat(numbers));
 
 // Consider the variable:
 
 // var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
 
+var numSet = [2, 13, 6, 8, 4, 2]
+console.log(numSet.indexOf(2))
+
 // Write the code that finds the index of the last appearance of the number 2.
 
+var numSet = [2, 13, 6, 8, 4, 2]
+console.log(numSet.lastIndexOf(2))
+
 // Write the code that returns the number at the third index.
+
+var numSet = [2, 13, 6, 8, 4, 2]
+console.log(numSet[3])
 
 // Consider the variable:
 
