0ffba4cbb899c50131960838c9b970f45236379d
Bpompa95 <brianpompa02@gmail.com>
2021-07-08 11:48:43 -0700

    completed 2 questions

--- javascript-conditionals-yarenny-brian.js
+++ javascript-conditionals-yarenny-brian.js
@@ -1,4 +1,16 @@
-var item 
-if(<=100){
-    console.log(in budget)
-}
+// 1. write an if/else statement that takes a variable of an item and logs "in budget" if a price is $100 or less?
+// var number = 100
+// if(number === 100){
+//     console.log('in budget')
+// } else if(number > 100 && number < 'in budget');
+
+// 2.write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+// var hungry = true
+// if(hungry === true) {
+//     console.log("eat food");
+// } else{
+//     console.log("keep coding");
+// }
+
+// Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
