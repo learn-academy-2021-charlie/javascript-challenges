72943a4180119008f9a5cd10ba4d7dcb1d85856b
christophermackey <c.mackey@me.com>
2021-07-08 16:32:27 -0700

    added even more challenges

--- javascript-arrays-christopher-alfonso.js
+++ javascript-arrays-christopher-alfonso.js
@@ -35,3 +35,33 @@ console.log(numSet.indexOf(2))
 console.log(numSet.lastIndexOf(2))
 // Write the code that returns the number at the third index.
 console.log(numSet[3])
+
+var characters = ["y", "a", "r", "r", "a"]
+// Write the code that brings all the letters in the characters array together into a string.
+var joinedCharacters = characters.join("-")
+console.log(joinedCharacters)
+// Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
+var charsReversed = characters.reverse()
+console.log(charsReversed)
+// Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
+var charsReversed2 = charsReversed.join("*")
+console.log(charsReversed2)
+// Write the code that brings all the letters in the charsReversed array together into a string without separators.
+var charsReversed3 = charsReversed.join("")
+console.log(charsReversed3)
+// Create two arrays consisting of three first names of your cohort members in each.
+var listOfNames1 = ["Christopher", "Alfonso", "Conrad"]
+var listOfNames2 = ["Brian", "Chauncy", "Heather"]
+// Write the code that sorts the names in alphabetical order.
+listOfNames1.sort()
+console.log(listOfNames1)
+console.log(listOfNames2.sort())
+// Write the code that sorts the names in reverse alphabetical order.
+listOfNames1.reverse()
+console.log(listOfNames1)
+listOfNames2.reverse()
+console.log(listOfNames2)
+// Write the code that sorts all the names in alphabetical order in a single array.
+
+var fullListOfNames = listOfNames1.concat(listOfNames2)
+console.log(fullListOfNames.sort())
