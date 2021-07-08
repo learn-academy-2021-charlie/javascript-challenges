c37a05974b689ae10093f72e713930d7bceccd07
Echo5Mike <1Raul.Marquez@gmail.com>
2021-07-08 11:39:16 -0700

    completed Q2

--- intro-js-r-t.js
+++ intro-js-r-t.js
@@ -1,8 +1,17 @@
 // Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
-var item = (1000)
+var item = (15)
 
 if (item < 100) {
     console.log("in Budget");
 } else {
     console.log ("out of budget")
+}
+
+//Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry = ("yes")
+
+if (hungry === "yes") {
+    console.log("Lets Eat");
+} else {
+    console.log ("Keep Coding")
 }
