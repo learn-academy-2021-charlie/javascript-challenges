058b10a06ee40c74db5f31268a3b94f097e4b195
ToddComte <comtetodd@gmail.com>
2021-07-08 12:05:31 -0600

    completed question 1

---
+++ intro-js-r-t.js
@@ -0,0 +1,8 @@
+// Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+var item = (1000)
+
+if (item < 100) {
+    console.log("in Budget");
+} else {
+    console.log ("out of budget")
+}
