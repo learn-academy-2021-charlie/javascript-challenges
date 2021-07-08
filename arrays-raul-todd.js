ff3448f9cfd438512119468220a1296c4a023b95
ToddComte <comtetodd@gmail.com>
2021-07-08 17:39:15 -0600

    complted 5-8

--- arrays-raul-todd.js
+++ arrays-raul-todd.js
@@ -14,3 +14,21 @@ console.log(groceryList.slice(0,2))
 groceryList.pop(groceryList.pop())
 groceryList.push("beans")
 console.log(groceryList)
+
+// Write the code that will add the number 0 to the beginning of the array.
+var numbers = [2, 4, 6, 8, 10]
+numbers.unshift(0)
+console.log(numbers)
+
+// Write the code that will add the number 12 to the end of the array.
+numbers.push(12)
+console.log(numbers)
+
+// Write the code that will remove the first number from the array.
+numbers.shift()
+console.log(numbers)
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+numbers.push(0)
+numbers.reverse()
+console.log(numbers)
