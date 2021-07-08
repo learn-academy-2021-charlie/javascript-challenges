8a4d208108453c18da585a112b64e734bdcbbc47
kel-man <kyyafuso@gmail.com>
2021-07-08 16:04:10 -0700

    switching back to practice github!

--- array.js
+++ array.js
@@ -16,25 +16,43 @@ var newList = groceryList.slice(0,2)
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
 
-newList.push("beans")
- console.log(newList)
+// newList.push("beans")
+//  console.log(newList)
 
 
 // Consider the variable:
 var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
 //
+// numbers.unshift(0)
+// console.log(numbers)
+//
 // Write the code that will add the number 12 to the end of the array.
+//
+// numbers.push(12)
+// console.log(numbers)
 //
 // Write the code that will remove the first number from the array.
 //
+// numbers.shift()
+// console.log(numbers)
+//
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+//
+// console.log([0].concat(numbers))
+// console.log(numbers)
 //
 // Write the code that finds the index of the first appearance of the number 2.
 //
+// console.log(numbers.indexOf(2))
+//
 // Write the code that finds the index of the last appearance of the number 2.
 //
+// console.log(numbers.lastIndexOf(2))
+//
 // Write the code that returns the number at the third index.
+//
+
 //
 // Write the code that brings all the letters in the characters array together into a string.
 //
