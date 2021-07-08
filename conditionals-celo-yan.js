e0dfc7d5da2be1dd6c33bddb85eaf1b04913ab15
celoazevedo <meazevedo86@gmail.com>
2021-07-08 10:57:30 -0700

    complete 1 - 3

---
+++ conditionals-celo-yan.js
@@ -0,0 +1,49 @@
+// Conditionals Challenge
+
+// Challenges
+// Make sure you try different options and change the variables to ensure properly working code.
+
+// Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+let item = 100;
+
+if (item < 100) {
+    console.log("in budget")
+} else {
+    console.log("not in budget!!!")
+}
+
+// Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+let feeling = 'hungry';
+if (feeling == 'hungry') {
+    console.log('eat food')
+} else {
+    console.log('keep coding')
+}
+
+// Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+let trafficLight = 'yellow'
+
+if (trafficLight == 'green') {
+    console.log('GO!')
+} else if (trafficLight == 'red'){
+    console.log('Stop!!!')
+} else {
+    console.log('slow down!')
+}
+
+// Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+
+// Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+// STRETCH Challenges
+
+// Write an if/else statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+
+// Write an if/else statement that takes a variable of a boolean, number, or string datatype and logs the data type of the variable HINT: use the JS operator typeof.
+
+// Create a password checker using a single if/else statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
+
+
