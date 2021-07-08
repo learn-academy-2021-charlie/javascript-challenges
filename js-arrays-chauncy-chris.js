c8e3f3834cd97324e6b28a3ad13ceea83f82e00d
Chrisluna10 <chrisluna4224@gmail.com>
2021-07-08 16:21:22 -0700

    problems section 3

--- js-arrays-chauncy-chris.js
+++ js-arrays-chauncy-chris.js
@@ -21,3 +21,35 @@ console.log(chipsNDips)
 chipsNDips.push("beans")
 console.log(chipsNDips)
 
+// Consider the variable:
+ var numbers = [2, 4, 6, 8, 10]
+
+ // Write the code that will add the number 0 to the beginning of the array.
+ numbers.unshift(0);
+ console.log(numbers);
+
+// Write the code that will add the number 12 to the end of the array.
+numbers.push(12);
+console.log(numbers);
+
+// Write the code that will remove the first number from the array.
+numbers.shift();
+console.log(numbers);
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var newNumbers = [0];
+newNumbers = newNumbers.concat(numbers);
+
+console.log(newNumbers);
+
+var numSet = [2, 13, 6, 8, 4, 2]
+
+// Write the code that finds the index of the first appearance of the number 2.
+console.log(numSet.indexOf(2))
+
+// Write the code that finds the index of the last appearance of the number 2.
+console.log(numSet.lastIndexOf(2))
+
+// Write the code that returns the number at the third index.
+console.log(numSet[3])
+
