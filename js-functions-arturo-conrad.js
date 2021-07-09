3958b1e2e863fdfda9a2dc0f0737008acbc21fff
charper95 <charper1995@gmail.com>
2021-07-09 12:04:47 -0700

    finished questions 6 through 9

--- js-functions-arturo-conrad.js
+++ js-functions-arturo-conrad.js
@@ -27,21 +27,79 @@
 // console.log(triple(6))
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
-const multiply = (numb1,numb2) => {
-  return numb1 * numb2
-}
-console.log(multiply(3,5))
+// const multiply = (numb1,numb2) => {
+//   return numb1 * numb2
+// }
+// console.log(multiply(3,5))
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-//
+// const divisibleBy = (num1, num2) => {
+//     if (num1 === num2) {
+//       return `${num1} and ${num2} are the same`
+//     } else if (num1%num2 === 0) {
+//       return `${num1} is evenly divisible by ${num2}`
+//     } else {
+//       return `${num1} is not evenly divisible by ${num2}`
+//     }
+// }
+
+// console.log(divisibleBy(65, 2))
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
-//
+// const assignGrade = (score) => {
+//   if (score === 100) {
+//     return "this is a perfect score"
+//   } else if (score >= 90) {
+//     return "A"
+//   } else if (score >= 80) {
+//     return "B"
+//   } else if (score >= 70) {
+//     return "C"
+//   } else if (score >= 60) {
+//     return "D"
+//   } else {
+//     return "F"
+//   }
+// }
+
+// console.log(assignGrade(100))
+// console.log(assignGrade(95))
+// console.log(assignGrade(85))
+// console.log(assignGrade(75))
+// console.log(assignGrade(65))
+// console.log(assignGrade(15))
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
-//
+// const isLonger = (string1, string2) => {
+//   if (string1.length > string2.length) {
+//     return string1 
+//   } else if (string1.length < string2.length) {
+//     return string2
+//   } else {
+//     return "they are the same"
+//   }
+// }
+
+// console.log(isLonger("longer", "short"))
+// console.log(isLonger("short", "longer"))
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
-//
+// const greaterNum = (num1, num2) => {
+//   if (num1 > num2) {
+//     return num1
+//   } else if (num1 < num2) {
+//     return num2
+//   } else {
+//     return "they are equal"
+//   }
+// }
+
+// console.log(greaterNum(2, 5))
+// console.log(greaterNum(8, 5))
+// console.log(greaterNum(5, 5))
 // Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
-//
+const yelling = (string) => {
+  string.toUppercase(); return string
+}
+
+console.log(yelling("yelling"))
 // STRETCH Challenges
 // The World Translator
 // (a) Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
