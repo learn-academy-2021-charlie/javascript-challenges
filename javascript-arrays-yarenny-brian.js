4f6f4ac4ff2d24f80ed6d78a2fa10b0636a6e2e3
byarenny <byarenny@gmail.com>
2021-07-09 11:24:56 -0700

    questions 1-5 completed

--- javascript-arrays-yarenny-brian.js
+++ javascript-arrays-yarenny-brian.js
@@ -1,9 +1,11 @@
  //1. Write the code that will add "soda" to the end of the original array.
+// Write the code that will add "soda" to the end of the original array.
 // var grocerylist = ["chips", "dip", "cookies"]
 // console.log(grocerylist.push("soda"))
 // console.log(grocerylist)
 
 // 2. Write the code that will add "granola" to the end of array without altering the original array.
+// Write the code that will add "granola" to the end of array without altering the original array.
 // var grocerylist1 = ["chips", "dip", "cookies"]
 // var grocerylist2 = ["granola"]
 // console.log(grocerylist1.concat(grocerylist2));
@@ -65,3 +67,19 @@ var oddIndexes = []
 // 19. Write the code that logs the values from the numbers array that are at odd indexes.
 
 // 20. Write the code that adds the values from odd indexes into the oddIndexes array..
+
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+// var grocerylist = ["chips", "dip", "cookies",]
+// console.log(grocerylist.slice(0, 2))
+
+// Write the code that will add "beans" to the "chips" and "dip" array.
+// var grocerylist = ["chips", "dip", "cookies"]
+// console.log(grocerylist.join("beans"))
+
+// Write the code that will add the number 0 to the beginning of the array.
+
+// Write the code that will add the number 12 to the end of the array.
+
+// Write the code that will remove the first number from the array.
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
