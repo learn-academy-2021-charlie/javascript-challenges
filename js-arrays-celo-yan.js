133620b753a9e52d3b1c4c2a2386c3a9f4254ae3
celoazevedo <meazevedo86@gmail.com>
2021-07-08 17:10:40 -0700

    complete challenge

--- js-arrays-celo-yan.js
+++ js-arrays-celo-yan.js
@@ -88,6 +88,11 @@ var numbers = [42, 221, 71, 7, 18, 87]
 var oddIndexes = []
 // Write the code that logs the values from the numbers array that are at odd indexes.
 
-// console.log()
+for (let i = 1; i < numbers.length; i++) {
+    if (i % 2 != 0) {
+        oddIndexes.push(numbers[i])
+    }
+}
+console.log(oddIndexes)
 
 // Write the code that adds the values from odd indexes into the oddIndexes array.
