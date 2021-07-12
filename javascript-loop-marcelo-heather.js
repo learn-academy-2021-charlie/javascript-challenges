80fd0f191dec7db3ad9177d71313c4b5a3311c0e
celoazevedo <meazevedo86@gmail.com>
2021-07-12 16:18:08 -0700

    complete chellenges

--- javascript-loop-marcelo-heather.js
+++ javascript-loop-marcelo-heather.js
@@ -42,8 +42,42 @@ for(let i=0; i<nums.length; i++){
   console.log(nums[i]%2)
 }
 // Looping over a string. Consider this variable:
-// var myString = "learn student"
+var myString = "learn student"
 // Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
+let counter = 0;
+for(let i = 0; i < myString.length; i++) {
+    if (myString[i] === 'e') {
+        counter += 1;
+    } else {
+        console.log(myString[i])
+    }
+}
+console.log(counter)
+
 // STRETCH Challenges
-// Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
-// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+// Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, 
+// and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
+for (let i = 0; i < 16; i ++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even`)
+    } else {
+        console.log(`${i} is odd`)
+    }
+}
+
+// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, 
+// replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. 
+// If a number is a multiple of both 3 and 5, replace it with fizzbuzz. 
+// Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+
+for (let i = 0; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FIZZ BUZZ')
+    } else if (i % 5 === 0) {
+        console.log('BUZZ')
+    } else if (i % 3 === 0) {
+        console.log('FIZZ')
+    } else {
+        console.log(i)
+    }
+}
