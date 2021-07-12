7c04efbb699b17e42464a0a6b119a61f0349ef62
ArtyG-10 <a.gourent@outlook.com>
2021-07-12 19:19:04 -0400

    finished second set of challenges

--- js-loops-arturo-christopher.js
+++ js-loops-arturo-christopher.js
@@ -10,11 +10,31 @@
 // for(let i=1; i<21; i++) {
 //     console.log(i*3)
 // }
+//
+// for(let i=1; i<21; i++) {
+//     if(i % 2 !== 0) {
+//         console.log("Odd");
+//     } else {
+//         console.log(i);
+//     }
+// }
+// Looping over an array. Consider this variable:
+var nums = [3, 57, -9, 20, 67]
+// // // Create a loop that will log the highest number from the array. Expected output --> 67
+// //   var highestNum = nums[0]
+// // for (let i=0; i <nums.length ; i++) {
+// //     if (highestNum < nums[i]) {highestNum = nums[i]}
+// // }
+// // console.log(highestNum)
+//
+// // Create a loop that will log the lowest number from the array Expected output --> -9
+//   var lowestNum = nums[0]
+// for (let i=0; i <nums.length ; i++) {
+//     if (nums[i] < lowestNum) {lowestNum = nums[i]}
+// }
+// console.log(lowestNum)
 
-for(let i=1; i<21; i++) {
-    if(i % 2 !== 0) {
-        console.log("Odd");
-    } else {
-        console.log(i);
-    }
-}
+// Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+ for (i=0; i<nums.length ; i++) {
+   console.log(nums[i] % 2) 
+ }
