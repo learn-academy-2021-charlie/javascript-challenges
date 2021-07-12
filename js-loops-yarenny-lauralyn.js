a8de4f8a995a96e4e48690dd9c69270c3ecba038
byarenny <byarenny@gmail.com>
2021-07-12 16:03:17 -0700

    challenges 1-2 completed

---
+++ js-loops-yarenny-lauralyn.js
@@ -0,0 +1,51 @@
+// Write a for loop that logs each number from 1 - 20.
+// Write a for loop that logs the result of each number from 1 - 20 tripled.
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
+
+// for(let i =1; i <=20; i ++){
+//     // console.log(i)
+//     // console.log(i * 3)
+//     if(i % 2 === 0){
+//     console.log (i)
+//     } else { console.log("odd")
+
+//     } 
+
+// } 
+// Looping over an array. Consider this variable:
+var nums = [3, 57, -9, 20, 67]
+// Create a loop that will log the highest number from the array. Expected output --> 67
+// Create a loop that will log the lowest number from the array Expected output --> -9
+// Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+ 
+// for(let i=0; i<nums.length; i++){
+//     let remainder = nums [i] % 2
+//     console.log(remainder)
+// }
+
+
+
+// var highest = nums[0]
+// for(let i= 1; i<nums.length; i++){
+//     let currentNum = nums[i]
+//     // console.log(currentNum)
+//     if(currentNum > highest) {
+//         highest = currentNum
+//         // console.log (`new highest is ${highest}`)
+//     }
+// }   
+//     console.log(highest)
+
+//     var highest = nums[0]
+// for(let i= 1; i<nums.length; i++){
+//     let currentNum = nums[i]
+//     // console.log(currentNum)
+//     if(currentNum > highest) {
+//         highest = currentNum
+//         // console.log (`new highest is ${highest}`)
+//     }
+// }   
+//     console.log(highest)
+
+
+
