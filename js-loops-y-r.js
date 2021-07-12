7f4fe7fb5f77a62cb2d10e77d7baece42f2e6aac
Echo5Mike <1Raul.Marquez@gmail.com>
2021-07-12 16:43:11 -0700

    one more to finish

--- js-loops-y-r.js
+++ js-loops-y-r.js
@@ -44,12 +44,30 @@ for(let i=0; i<nums.length; i++){
     console.log(nums[i] % 2)
 }
 // 3. Looping over a string. Consider this variable:
-// ```javascript
-// var myString = "learn student"
-// ```
+var myString = "learn student"
+var count = 0
 // - Write the code that will log the number of times the letter "e" occurs in the string. **Expected output** --> 2
+for (let i = 0; i<myString.length; i++){
+    if ([myString[i]] == "e"){
+        count = count +1
+    }
+}
+console.log(count)
+
 
 // ### STRETCH Challenges
 
 // 1. Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. **Expected output** --> "0 is even" "1 is odd" "2 is even" ...etc
+
+for(let i=0; i<=15; i++){
+    if(i===0){
+        console.log(i + " IS EVEN")
+    }else if(i % 2 != 0){
+        console.log(i + " IS ODD")
+    }else{
+        console.log(i + " IS EVEN")
+    }
+}
+
+
 // 2. Fizz Buzz: Use a for loop to log all numbers from 1-100.  If a number is a multiple of 3, replace it with the word `fizz`. If a number is a multiple of five, replace it with the word `buzz`. If a number is a multiple of both 3 and 5, replace it with `fizzbuzz`. **Expected output** --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
