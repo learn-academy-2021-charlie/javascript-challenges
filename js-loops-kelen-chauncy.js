054e63006517b878637565b61b14cc058fc77e07
kel-man <kyyafuso@gmail.com>
2021-07-12 15:58:30 -0700

    remaining stretch challenges completed

--- js-loops-kelen-chauncy.js
+++ js-loops-kelen-chauncy.js
@@ -19,13 +19,13 @@ for(let i = 0;i <= 20; i++){
     else{
         console.log("ODD");
     }
-    
+
 }
 console.log("--------------------------------------------------")
 
 // Looping over an array. Consider this variable:
  var nums = [3, 57, -9, 20, 67]
- 
+
 // Create a loop that will log the highest number from the array. Expected output --> 67
 var highest;
 for(let i = 0; i < nums.length; i++){
@@ -71,4 +71,23 @@ console.log(count);
 console.log("--------------------------------------------------")
 // STRETCH Challenges
 // Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
-// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+//
+for(let i = 0; i<=15; i++){
+  if(i%2 == 0){ console.log(`${i} is even`)}
+  if(i%2 !== 0){ console.log(`${i} is odd`)}
+}
+//
+// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five,
+// replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7,
+// 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+//
+
+for(let i = 0; i <= 100; i++){
+  if(i%15 != 0){
+    if(i%3==0){
+      console.log('fizz')
+    }else if(i%5==0){
+      console.log('buzz')
+    } else {console.log(i)}
+  } else {console.log('fizzbuzz')}
+}
