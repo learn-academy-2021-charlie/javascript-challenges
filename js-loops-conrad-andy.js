686ae963cecaed0108d306a00ec6ffa907929a3c
charper95 <charper1995@gmail.com>
2021-07-12 16:54:19 -0700

    changes

--- js-loops-conrad-andy.js
+++ js-loops-conrad-andy.js
@@ -8,10 +8,10 @@
 //     console.log(i * 3)
 // }
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
-for(let i = 1; i <= 20; i++) {
-    if(i % 2 !== 0) {
-        console.log("ODD")
-    } else {
-        console.log(i)
-    }
-}
+// for(let i = 1; i <= 20; i++) {
+//     if(i % 2 !== 0) {
+//         console.log("ODD")
+//     } else {
+//         console.log(i)
+//     }
+// }
