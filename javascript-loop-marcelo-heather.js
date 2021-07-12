cc4c4c457d1ad5320ab8f66b91976537f983f308
hroulston <h_roulston@live.com>
2021-07-12 16:03:45 -0700

    next set of changes

--- javascript-loop-marcelo-heather.js
+++ javascript-loop-marcelo-heather.js
@@ -11,7 +11,7 @@ for (let i = 1; i < 21; i++) {
     console.log(i * 3)
 }
 
-// Create a for loop that logs each even number from 1-20, and in the place of every odd number, 
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number,
 // returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
 for (let i = 1; i < 21; i++) {
     if (i % 2 !== 0) {
@@ -22,10 +22,25 @@ for (let i = 1; i < 21; i++) {
 }
 
 // Looping over an array. Consider this variable:
-// var nums = [3, 57, -9, 20, 67]
+var nums = [3, 57, -9, 20, 67]
 // Create a loop that will log the highest number from the array. Expected output --> 67
+let maxHeight = 0;
+for(let i = 0; i < nums.length; i ++){
+  if(nums[i] > maxHeight){
+    maxHeight = nums[i]
+  }
+}
 // Create a loop that will log the lowest number from the array Expected output --> -9
+let minHeight = 0;
+for(let i = 0; i < nums.length; i ++){
+  if(nums[i] < minHeight){
+    minHeight = nums[i]
+  }
+}
 // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+for(let i=0; i<nums.length; i++){
+  console.log(nums[i]%2)
+}
 // Looping over a string. Consider this variable:
 // var myString = "learn student"
 // Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
