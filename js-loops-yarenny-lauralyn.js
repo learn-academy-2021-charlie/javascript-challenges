4ae610c10f8f99622abaf9ed8a4be7acf9244965
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-12 16:40:59 -0700

    3 and stretch done

--- js-loops-yarenny-lauralyn.js
+++ js-loops-yarenny-lauralyn.js
@@ -36,16 +36,91 @@ var nums = [3, 57, -9, 20, 67]
 // }   
 //     console.log(highest)
 
-//     var highest = nums[0]
+
+// var nums = [3, 57, -9, 20, 67]
+
+// first set lowest = 3
+// i = 1, currentNum = 57, 57 < 3? no, skip it
+// i = 2, currNum = -9, -9 < 3? yes! reassign lowest, so lowest = -9
+// i = 3, currNum = 20, 20 < -9? no! keep going
+// i = 4, currNum = 67, 67 < -9? no!
+// i = 5, but 5 < 5 for i < nums.length. so we stop.
+
+// var lowest = nums[0]
+
 // for(let i= 1; i<nums.length; i++){
-//     let currentNum = nums[i]
-//     // console.log(currentNum)
-//     if(currentNum > highest) {
-//         highest = currentNum
-//         // console.log (`new highest is ${highest}`)
+//     console.log(`i = ${i}, lowest = ${lowest}`)
+//     console.log(`checking ${nums[i]} < ${lowest}`)
+//     if(nums[i] < lowest) {  // check if current num is less than lowest
+//         lowest = nums[i]    // reassign current num as the new lowest num
+//         console.log (`new lowest is ${lowest}`)
 //     }
 // }   
-//     console.log(highest)
+
+// console.log(lowest)
+
+
+// 3. Looping over a string. Consider this variable:
+var myString = "learn student"
+// Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
+
+let occurrencesOfE = 0
+
+for (let i = 0; i < myString.length; i++) {
+    // console.log(`current letter is ${myString[i]}`)
+    if (myString[i] === 'e') {
+        // console.log(`found an e!`)
+        // console.log(occurrencesOfE)
+        // occurrencesOfE = occurrencesOfE + 1
+        occurrencesOfE++
+    }
+}
+
+// console.log(occurrencesOfE)
 
 
 
+// STRETCH Challenges
+// Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
+
+// for loop i = 0; i <= 15
+// check if curr num is odd or even
+// if num is even, output num is even
+// if num is odd, output num is odd
+
+for (let i = 0; i <= 15; i++) {
+    // console.log(i)
+    if (i % 2 === 0) {  // i is even
+        console.log(`${i} is even`)
+    } else {    // i is odd
+        console.log(`${i} is odd`)
+    }
+}
+
+
+// Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+
+// a number is a multiple of 5 if it evenly divides by that number.
+// 5 / 5 = 1, r 0
+// 10 / 5 = 2, r 0
+// 15 / 3 = 5, r 0
+// 3 / 3 = 1, r 0
+// 4 / 3 = 1 r 1, 4 is not a multiple of 3
+
+for (let i = 1; i <= 100; i++) {
+    // check if num is a multiple of 3, if so, log fizz
+    if (i % 3 === 0) {
+        console.log("fizz")
+    } else if (i % 5 === 0) { 
+        // check if num is a multiple of 5, if so, log buzz
+        console.log("buzz")
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        // check if num is a multiple of 3 and 5, if so, log fizzbuzz
+        console.log("fizzbuzz")
+    } else {
+        // otherwise log that number
+        console.log(i)
+    }
+    
+}   
+ 
