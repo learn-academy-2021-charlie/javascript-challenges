7c9ff03626789d0ee6a15253cd71668abdca1486
Andy19936 <andy19936@gmail.com>
2021-07-12 20:22:03 -0700

    zero fix final version

--- js-loops-conrad-andy.js
+++ js-loops-conrad-andy.js
@@ -79,27 +79,34 @@ var myString = "learn student"
 
 //     Even or Odd: Write a for loop that iterates from 0 to 15. For each iteration, it will check if the current number is odd or even, and display the appropriate outcome. Expected output --> "0 is even" "1 is odd" "2 is even" ...etc
 
+
+
+
 for(let i= 0; i<=15; i++) {
-    if(i % 2 == 1 ){
+    if(i === 0 ){  
+        console.log(`0`)
+   }else if(i % 2 == 1 ){
     console.log(`${i} is odd`) 
-    } else if(i %2 == 0){   
+    } else if(i % 2 == 0){   
     console.log(`${i} is even`)
-    }
-    // } else (i==0) 
-    //     console.log(`0`)
-    
+    }  
 }
+
+
+
+
+
 
 // Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
 
-for(let i=1; i<=100; i++) {
-    if (i % 3 ==0 && i % 5 ==0){
-        console.log("fizzbuzz")
-    } else if(i % 3 == 0 ){
-        console.log("fizz")
-    } else if(i % 5 == 0){
-        console.log("buzz")
-    } else {
-        console.log(`${i}`)
-    }
-}
+// for(let i=1; i<=100; i++) {
+//     if (i % 3 ==0 && i % 5 ==0){
+//         console.log("fizzbuzz")
+//     } else if(i % 3 == 0 ){
+//         console.log("fizz")
+//     } else if(i % 5 == 0){
+//         console.log("buzz")
+//     } else {
+//         console.log(`${i}`)
+//     }
+// }
