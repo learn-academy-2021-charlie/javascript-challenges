3d2a53f5521d8df251d72651486467b2bab20b49
charper95 <charper1995@gmail.com>
2021-07-12 18:23:10 -0700

    changes

--- js-loops-conrad-andy.js
+++ js-loops-conrad-andy.js
@@ -1,13 +1,19 @@
-// Logging values with for loops
+// 1. Logging values with for loops
+
 // Write a for loop that logs each number from 1 - 20.
+
 // for(let i = 1; i <= 20; i++) {
 //     console.log(i)
 // }
+
 // Write a for loop that logs the result of each number from 1 - 20 tripled.
+
 // for(let i = 1; i <= 20; i++) {
 //     console.log(i * 3)
 // }
+
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
+
 // for(let i = 1; i <= 20; i++) {
 //     if(i % 2 !== 0) {
 //         console.log("ODD")
@@ -15,3 +21,58 @@
 //         console.log(i)
 //     }
 // }
+
+// 2. Looping over an array. Consider this variable:
+
+// var nums = [3, 57, -9, 20, 67, -5, -15]
+
+// Create a loop that will log the highest number from the array. Expected output --> 67
+
+// var acc = 0
+
+// for(let i = 0; i < nums.length; i++) {
+//     // if the value of the element of the array is greater than zero.
+//     if (nums[i] > acc) {
+//         acc = nums[i];
+//     }
+// }
+
+// console.log(acc);
+
+// Create a loop that will log the lowest number from the array Expected output --> -9
+
+// var nums = [3, 57, -9, 20, 67, -5, -15]
+
+// var acc = 0
+
+// for(let i = 0; i < nums.length; i++) {
+//     if (nums[i] < acc) {
+//         acc = nums[i];
+//         // console.log(acc); output --> -9 -15
+//     }
+//     //console.log(acc); output --> 0 0 -9 -9 -9 -9 -15
+// }
+// console.log(acc); // output --> -15
+
+
+// Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+
+// var nums = [3, 57, -9, 20, 67]
+
+// for(let i = 0; i < nums.length; i++) {
+//     console.log(nums[i] % 2)
+// }
+
+// 3. Looping over a string. Consider this variable:
+
+var myString = "learn student"
+
+// Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
+
+    // var letterCount = 0;
+    // for (let i = 0; i < myString.length; i++) {
+    //     if (myString.charAt(i) == "e") {
+    //         letterCount += 1;
+    //     }
+    // }
+    // console.log(letterCount)
