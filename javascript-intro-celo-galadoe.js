3225f90f6614f5df3838e3f24ef9fa5e258e7f29
Mina Chuong <mina.chuong@gmail.com>
2021-07-16 11:52:33 -0700

    Revert "Complete 1st part"
    This reverts commit ca06acd152d114342d46ce62d1b365c4c868d800.

---
+++ javascript-intro-celo-galadoe.js
@@ -0,0 +1,85 @@
+// Challenges
+
+// 1
+console.log(34+71)
+
+// 2
+ console.log(67-123)
+
+ // 3
+ console.log(56*23)
+
+ // 4
+ console.log(45/5)
+
+ // 5
+ console.log(5**7)
+
+ // 6
+ console.log(33 % 6)
+
+// 7
+console.log("galadoe".length);
+
+// 8
+console.log("galadoe".includes("e"));
+
+// 9
+console.log("galadoe"[0]);
+
+// 10
+console.log("galadoe".toUpperCase());
+
+// 11
+console.log((34/3) > (67/2))
+
+// 12
+console.log(5 == "5")
+
+// 13
+console.log(5 === "5")
+
+// 14
+console.log(!3 === 3)
+
+// 15
+console.log("LEARN".length === 5 && "Student".length === 7)
+
+// 16
+console.log("LEARN".length === 5 || "Student".length === 10);
+
+// 17
+console.log("LEARN".includes("RN"));
+
+// 18
+console.log("LEARN".includes("rn"));
+
+// 19
+console.log("LEARN"[0] === "l");
+
+// 20
+console.log("LEARN"[0] === "L");
+
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+
+// 21
+console.log(theAnswer / 2)
+
+// 22
+console.log(theQuestion.length)
+
+// 23
+console.log(theQuestion.indexOf("f"))
+
+// 24 --- maybe type coersion is converting theAnswer data type from a number into a string
+console.log(theQuestion + theAnswer)
+
+// 25
+console.log(theQuestion.slice(6, 18))
+
+// 26
+console.log(theQuestion[0]);
+
+// 27
+console.log(theQuestion.length > theAnswer);
